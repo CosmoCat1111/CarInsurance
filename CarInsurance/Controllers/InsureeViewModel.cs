f2b0dd5f1b90eea3d28897e675a3ec53307e1e18
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-23 11:13:33 -0700

    Add project files.

---
+++ CarInsurance/Controllers/InsureeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarInsurance.Controllers
+{
+    public class InsureeViewModel
+    {
+        public string CarModel { get; internal set; }
+        public int Age { get; internal set; }
+    }
+}
